51135ed2b5f1e1730f405e9de9d2f0f10a64acba
AngryCracker <ads6may@gmail.com>
2017-06-05 17:13:04 +0530

    Made path

---
+++ Car AI 3/Assets/Scripts/Path.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour {
+
+    public Color color;
+    private List<Transform> nodes;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = color;
+        Transform[] pathTransforms = GetComponentsInChildren<Transform>();
+        nodes = new List<Transform>();
+        for(int i = 0; i < pathTransforms.Length; ++i)
+        {
+            if(pathTransforms[i] != this.transform)
+            {
+                nodes.Add(pathTransforms[i]);
+            }
+        }
+
+        for(int i = 0; i < nodes.Count; ++i)
+        {
+            Vector3 current = nodes[i].position;
+            Vector3 previous = Vector3.zero;
+            if (i > 0) previous = nodes[i - 1].position;
+            else if (i == 0 &&  nodes.Count > 1) previous = nodes[nodes.Count - 1].position;
+            Gizmos.DrawLine(previous, current);
+            Gizmos.DrawWireSphere(current, 1f);
+        }
+    }
+}
