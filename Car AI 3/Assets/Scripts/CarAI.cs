71f791c8b24f890e97df3f809234d7507341e864
AngryCracker <ads6may@gmail.com>
2017-06-05 19:20:04 +0530

    Added brakes

--- Car AI 3/Assets/Scripts/CarAI.cs
+++ Car AI 3/Assets/Scripts/CarAI.cs
@@ -9,8 +9,12 @@ public class CarAI : MonoBehaviour {
     public float maxSpeed = 100f;
     public float maxMotorTorque = 100f;
     public float maxBrakingTorque = 200f;
-    public float maxSteerAngle = 45f;
+    public float maxSteerAngle = 60f;
     public Vector3 centerOfMass;
+    public bool isBraking = false;
+    public Texture2D normal;
+    public Texture2D braking;
+    public Renderer carTextureRenderer;
 
     public WheelCollider wheelfl;
     public WheelCollider wheelfr;
@@ -35,6 +39,7 @@ public class CarAI : MonoBehaviour {
         Steer();
         Drive();
         Next();
+        Brake();
 	}
 
     private void Steer()
@@ -43,13 +48,15 @@ public class CarAI : MonoBehaviour {
         float steer = (relative.x / relative.magnitude) * maxSteerAngle;
         wheelfl.steerAngle = steer;
         wheelfr.steerAngle = steer;
-        Debug.DrawLine(this.transform.position, nodes[current].position);
+        // Debug.DrawLine(this.transform.position, nodes[current].position);
+        if (Mathf.Abs(steer) >= 0.5 * maxSteerAngle && currentSpeed > maxSpeed) isBraking = true;
+        else isBraking = false;
     }
 
     private void Drive()
     {
         currentSpeed = 2 * Mathf.PI * wheelfl.radius * wheelfl.rpm * 60 / 1000;
-        if (currentSpeed < maxSpeed)
+        if (currentSpeed < maxSpeed && !isBraking)
         {
             wheelrl.motorTorque = maxMotorTorque;
             wheelrr.motorTorque = maxMotorTorque;
@@ -63,7 +70,7 @@ public class CarAI : MonoBehaviour {
 
     private void Next()
     {
-        if (Vector3.Distance(this.transform.position, nodes[current].position) < 5f)
+        if (Vector3.Distance(this.transform.position, nodes[current].position) < 7f)
         {
             if (current == nodes.Count - 1)
             {
@@ -73,6 +80,25 @@ public class CarAI : MonoBehaviour {
             {
                 current++;
             }
+        }
+    }
+
+    private void Brake()
+    {
+        if(isBraking)
+        {
+            carTextureRenderer.material.mainTexture = braking;
+            wheelfl.brakeTorque = maxBrakingTorque;
+            wheelfr.brakeTorque = maxBrakingTorque;
+            wheelrl.brakeTorque = maxBrakingTorque;
+            wheelrr.brakeTorque = maxBrakingTorque;
+        } else
+        {
+            carTextureRenderer.material.mainTexture = normal;
+            wheelfl.brakeTorque = 0;
+            wheelfr.brakeTorque = 0;
+            wheelrl.brakeTorque = 0;
+            wheelrr.brakeTorque = 0;
         }
     }
 }
---
+++ Car AI 3/Assets/Scripts/Wheels.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheels : MonoBehaviour {
+
+    public WheelCollider target;
+    private Vector3 position = new Vector3();
+    private Quaternion rotation = new Quaternion();
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        target.GetWorldPose(out position, out rotation);
+        this.transform.position = position;
+        this.transform.rotation = rotation;
+	}
+}
