6c76cee862a49c6c72a4304d0aa2094acb51c9ab
AngryCracker <ads6may@gmail.com>
2017-06-08 20:25:56 +0530

    Random maze generation working

--- Terrain Generation Sandbox/Assets/Scripts/Maze.cs
+++ Terrain Generation Sandbox/Assets/Scripts/Maze.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainData : MonoBehaviour {
+public class Maze : MonoBehaviour {
+
+    public GameObject V;
 
     Terrain terrain;
     UnityEngine.TerrainData tData;
@@ -12,13 +14,23 @@ public class TerrainData : MonoBehaviour {
 
     float[,] heights;
 
-	void Start () {
+    void Start()
+    {
         terrain = transform.GetComponent<Terrain>();
         tData = terrain.terrainData;
 
         xRes = tData.heightmapWidth;
         yRes = tData.heightmapHeight;
-	}
+
+        for(int x = 0; x <= 50; ++x)
+        {
+            for(int z = 1; z <= 50; ++z)
+            {
+                if(Random.Range(0, 100) < 30)
+                Instantiate(V, new Vector3(x*4 + 1, 0, z*4 + 1), Quaternion.identity);
+            }
+        }
+    }
 
     private void OnGUI()
     {
@@ -35,15 +47,15 @@ public class TerrainData : MonoBehaviour {
         for (int y = 0; y < yRes; ++y)
             for (int x = 0; x < xRes; ++x)
                 heights[x, y] = Mathf.PerlinNoise(x * 0.25f * strength, y * 0.25f * strength) * 0.1f;
-                //heights[x, y] = Random.Range(0.0f, strength) * 0.5f;
+        //heights[x, y] = Random.Range(0.0f, strength) * 0.5f;
         tData.SetHeights(0, 0, heights);
     }
 
     void resetPoints()
     {
         heights = tData.GetHeights(0, 0, xRes, yRes);
-        for(int y = 0; y < yRes; ++y)
-            for(int x = 0; x < xRes; ++x)
+        for (int y = 0; y < yRes; ++y)
+            for (int x = 0; x < xRes; ++x)
                 heights[x, y] = 0;
         tData.SetHeights(0, 0, heights);
     }
