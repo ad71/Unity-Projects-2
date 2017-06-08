a23ee9e21ba11f41b842cbdd7766c41187471f13
AngryCracker <ads6may@gmail.com>
2017-06-08 18:53:47 +0530

    Working on terrain generation

---
+++ Terrain Generation Sandbox/Assets/TerrainData.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainData : MonoBehaviour {
+
+    Terrain terrain;
+    UnityEngine.TerrainData tData;
+
+    int xRes;
+    int yRes;
+
+    float[,] heights;
+
+	// Use this for initialization
+	void Start () {
+        terrain = transform.GetComponent<Terrain>();
+        tData = terrain.terrainData;
+
+        xRes = tData.heightmapWidth;
+        yRes = tData.heightmapHeight;
+	}
+
+    private void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 25), "Wrinkle"))
+        {
+            randomizePoints(0.1f);
+        }
+
+        if (GUI.Button(new Rect(10, 40, 100, 25), "Reset"))
+        {
+            resetPoints();
+        }
+    }
+
+    void randomizePoints(float strength)
+    {
+        heights = tData.GetHeights(0, 0, xRes, yRes);
+        for(int y = 0; y < yRes; ++y)
+        {
+            for(int x = 0; x < xRes; ++x)
+            {
+                heights[x, y] = Random.Range(0.0f, strength) * 0.5f;
+            }
+        }
+        tData.SetHeights(0, 0, heights);
+    }
+
+    void resetPoints()
+    {
+        heights = tData.GetHeights(0, 0, xRes, yRes);
+        for(int y = 0; y < yRes; ++y)
+        {
+            for(int x = 0; x < xRes; ++x)
+            {
+                heights[x, y] = 0;
+            }
+        }
+        tData.SetHeights(0, 0, heights);
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Terrain Generation Sandbox/Assets/TerrainData.js
@@ -0,0 +1,51 @@
+﻿//#pragma strict
+
+//var terrain:Terrain;
+//var tData:TerrainData;
+//var xRes:int;
+//var yRes:int;
+//
+//var heights:float[];
+//
+//function Start () {
+//	terrain = transform.GetComponent(Terrain);
+//	tData = terrain.terrainData;
+//
+//	xRes = tData.heightmapWidth;
+//	yRes = tData.heightmapHeight;
+//	terrain.activeTerrain.heightmapMaximumLOD = 0;
+//}
+
+//function OnGUI() {
+//	if (GUI.Button(Rect(10, 10, 100, 25), "Wrinkle")) {
+		//randomizePoints(0.1);
+	//}
+	//if (GUI.Button(Rect(10, 40, 100, 25), "Reset")) {
+//		resetPoints();
+//	}
+//}
+//
+//function randomizePoints(strength:float) {
+//	var heights = tData.GetHeights(0, 0, xRes, yRes);
+//	for(y = 0; y < yRes; ++y) {
+		//for(x = 0; x < xRes; ++x) {
+//			heights[x, y] = Random.Range(0.0, strength)*0.5;
+		//}
+	//}
+	//tData.SetHeights(0, 0, heights);
+//}
+//
+//function resetPoints() {
+//	var heights = tData.GetHeights(0, 0, xRes, yRes);
+//	for(y = 0; y < yRes; ++y) {
+		//for(x = 0; x < xRes; ++x) {
+//			heights[x, y] = 0;
+		//}
+	//}
+	//tData.SetHeights(0, 0, heights);
+//}
+//
+//function Update () {
+//	
+//}
+//////
