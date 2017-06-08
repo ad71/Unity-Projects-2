5edb94a4f891c93312349a554df810441ca1ae24
AngryCracker <ads6may@gmail.com>
2017-06-08 19:00:10 +0530

    minor

--- Terrain Generation Sandbox/Assets/TerrainData.cs
+++ Terrain Generation Sandbox/Assets/TerrainData.cs
@@ -12,7 +12,6 @@ public class TerrainData : MonoBehaviour {
 
     float[,] heights;
 
-	// Use this for initialization
 	void Start () {
         terrain = transform.GetComponent<Terrain>();
         tData = terrain.terrainData;
@@ -24,26 +23,18 @@ public class TerrainData : MonoBehaviour {
     private void OnGUI()
     {
         if (GUI.Button(new Rect(10, 10, 100, 25), "Wrinkle"))
-        {
             randomizePoints(0.1f);
-        }
 
         if (GUI.Button(new Rect(10, 40, 100, 25), "Reset"))
-        {
             resetPoints();
-        }
     }
 
     void randomizePoints(float strength)
     {
         heights = tData.GetHeights(0, 0, xRes, yRes);
         for(int y = 0; y < yRes; ++y)
-        {
             for(int x = 0; x < xRes; ++x)
-            {
                 heights[x, y] = Random.Range(0.0f, strength) * 0.5f;
-            }
-        }
         tData.SetHeights(0, 0, heights);
     }
 
@@ -51,17 +42,8 @@ public class TerrainData : MonoBehaviour {
     {
         heights = tData.GetHeights(0, 0, xRes, yRes);
         for(int y = 0; y < yRes; ++y)
-        {
             for(int x = 0; x < xRes; ++x)
-            {
                 heights[x, y] = 0;
-            }
-        }
         tData.SetHeights(0, 0, heights);
     }
-
-    // Update is called once per frame
-    void Update () {
-		
-	}
 }
