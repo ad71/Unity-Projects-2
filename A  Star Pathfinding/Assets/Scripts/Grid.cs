eff008ecbefed6feb3a9402f091e33a362ec157b
AngryCracker <ads6may@gmail.com>
2017-06-09 14:02:01 +0530

    Added gizmos to visualize unwalkable layers

---
+++ A  Star Pathfinding/Assets/Scripts/Grid.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour {
+
+    private float nodeDiameter;
+    private int gridSizeX;
+    private int gridSizeY;
+    public LayerMask unwalkableMask;
+    public Vector2 gridworldSize;
+    public float nodeRadius;
+    Node[,] grid;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, new Vector3(gridworldSize.x, 1, gridworldSize.y));
+        if (grid != null)
+        {
+            foreach (Node n in grid)
+            {
+                Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                Gizmos.DrawCube(n.worldposition, Vector3.one * (nodeDiameter - 0.1f));
+            }
+        }
+    }
+
+    private void Start()
+    {
+        nodeDiameter = nodeRadius * 2;
+        gridSizeX = Mathf.RoundToInt(gridworldSize.x / nodeDiameter);
+        gridSizeY = Mathf.RoundToInt(gridworldSize.y / nodeDiameter);
+        CreateGrid();
+    }
+
+    private void CreateGrid()
+    {
+        grid = new Node[gridSizeX, gridSizeY];
+        Vector3 worldBottomLeft = this.transform.position - Vector3.right * gridworldSize.x / 2 - Vector3.forward * gridworldSize.y / 2;
+        for (int x = 0; x < gridSizeX; ++x)
+        {
+            for (int y = 0; y < gridSizeY; ++y)
+            {
+                Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
+                bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
+                grid[x, y] = new Node(walkable, worldPoint);
+            }
+        }
+    }
+}
---
+++ A  Star Pathfinding/Assets/Scripts/Node.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+
+    public bool walkable;
+    public Vector3 worldposition;
+
+    public Node(bool _walkable, Vector3 _worldPosition)
+    {
+        this.walkable = _walkable;
+        this.worldposition = _worldPosition;
+    }
+}
