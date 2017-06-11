fa95997cade6268be4536e77051e5be01b503123
AngryCracker <ads6may@gmail.com>
2017-06-11 15:27:47 +0530

    Prefab

---
+++ Car AI GA/Assets/Scripts/Wheel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wheel : MonoBehaviour {
+
+    public WheelCollider target;
+    private Vector3 position = new Vector3();
+    private Quaternion rotation = new Quaternion();
+	void Update () {
+        target.GetWorldPose(out position, out rotation);
+        this.transform.position = position;
+        this.transform.rotation = rotation;
+	}
+}
