e281b92ce26df9213315e7d5e7b07b228b60b746
AngryCracker <ads6may@gmail.com>
2017-06-11 16:33:02 +0530

    Added DNA class

--- Car AI GA/Assets/Scripts/DNA.cs
+++ Car AI GA/Assets/Scripts/DNA.cs
@@ -2,7 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DNA {
-    
+public class DNA : MonoBehaviour {
 
+    /* Parameters:
+     * maxSteerAngle
+     * topSpeed
+     * maxMotorTorque
+     * maxBrakingTorque
+     * centerOfMass.y
+     * sensorLength
+     * sensorSkewAngle
+     * 4WheelDrive?
+     * 4WheelBrake?
+     * 4WheelTurn?
+     * switchToNextWaypointDistance
+     * sense() ?
+     * brakingConditions
+     * avoidMultiplier parameter
+     */
+
+    List<float> genes;
+
+    private void Start()
+    {
+        genes = new List<float>();
+        for(int i = 0; i < 14; ++i)
+        {
+            genes.Add(Random.Range(0f, 1f));
+        }
+    }
 }
