d5951f2fd3584c35dd3da339a112a53dc5f0862f
AngryCracker <ads6may@gmail.com>
2017-06-11 17:16:10 +0530

    Camera follows without quaternion duplication

---
+++ Car AI GA/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Transform car;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Quaternion rotation = car.rotation;
+        rotation.z = 0;
+        this.transform.rotation = rotation;
+        this.transform.position = car.position - car.transform.forward * 1.5f + car.transform.up * 0.75f;
+	}
+}
